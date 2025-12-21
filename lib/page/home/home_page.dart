684b7c6e4ae5959ce95d957d290259fd38465cb7
joohyelim1208 <jhl01208@gmail.com>
2025-12-21 14:16:37 -0800

    faet: AppBar 필수 구현

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -3,9 +3,28 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(Object context) {
-    return AppBar(
-      // ! 이름 부분은 사용자가 입력하는 값에 맞춰서 변경이 되어야 한다.
-      title: ("혜림's Task", TextStyle()),
+    // Scaffold구조 안에 앱 바, 바디 구성
+    return Scaffold(
+      appBar: homeAppBar(),
+      body: ListView(children: [
+          
+        ],
+      ),
     );
   }
+}
+
+// 앱 바 필수 구현 완료
+AppBar homeAppBar() {
+  return AppBar(
+    title: Text(
+      // 필수. 수강생 이름 넣기
+      "혜림`s Tasks",
+      style: TextStyle(
+        // 필수. 사이즈와 볼드체 적용
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
 }
